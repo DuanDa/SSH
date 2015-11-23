9a3488426d24fd8ea7771f7a273b3201855f2221
Duan-Macbook <dansuninho@gmail.com>
2015-11-24 00:11:13 +0800

    1. Successfully insert data into DB;

--- src/com/duan/Main.java
+++ src/com/duan/Main.java
@@ -1,6 +1,10 @@
 package com.duan;
 
 import com.duan.inter.IChangeLetter;
+import com.duan.model.Employee;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
@@ -13,5 +17,15 @@ public class Main {
 		ApplicationContext context = new ClassPathXmlApplicationContext("spring-config.xml");
 		IChangeLetter upperCase = (IChangeLetter) context.getBean("upperCase");
 		System.out.println("---> result: " + upperCase.changeLetter());
+
+		Employee employee = new Employee();
+		employee.setId(1);
+		employee.setAge(20);
+		employee.setName("Tim");
+		SessionFactory sessionFactory = (SessionFactory) context.getBean("sessionFactory");
+		Session session = sessionFactory.openSession();
+		Transaction transaction = session.beginTransaction();
+		session.save(employee);
+		transaction.commit();
 	}
 }
